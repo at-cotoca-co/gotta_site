f5b15fd8716823d8f9d096bf5c435597c09007aa
at <at@cotoca.co>
2020-06-13 15:38:16 +0900

    Update api config.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_AKUKoE9Lz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 't291en7a35c2fkknec05t3ik6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_AKUKoE9Lz',
+        userPoolClientId: 't291en7a35c2fkknec05t3ik6',
+        region: 'ap-northeast-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://z8x95b4ajd.execute-api.ap-northeast-1.amazonaws.com/prod'
     }
 };
